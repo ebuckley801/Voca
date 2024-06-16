decc428fc057f0a7d19715d9b58cd34561042a15
Boris Kizenko <boriskizenko@Boriss-MacBook-Air.local>
2024-06-16 13:38:41 -0400

    blaahhhh

--- nextjs-app/utils/prompter.js
+++ nextjs-app/utils/prompter.js
@@ -1,4 +1,3 @@
-"use client"
 import { OpenAI } from 'openai';
 //import { createClient } from '@supabase/supabase-js';
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -13,18 +12,12 @@ export const FetchConjugations = async (callback) => {
   try {
     const completion = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
-      messages: [{role: "assistant", content: "List the conjugations of the verb 'to be' in Italian in the present tense."}],
+      messages: [{role: "assistant", content: "List one of the 10 most common verbs in Italian in its infinitive form. right next to it put its translation into English, and then in a table, provide the conjugation for the present tense"}],
       max_tokens: 100
     });
     console.log("API Response:", completion); // Log the full API response
     const content = completion.choices[0].message.content; // Log the first choice to see its contents
-    // if (completion.choices && completion.choices.length > 0 && completion.choices[0].text) {
-    //   const text = completion.choices[0].text.trim();
-    //   callback(text);  // Use the callback to update state
-    // } else {
-    //   console.error('No text available in the API response');
-    //   callback(''); // Handle the case where no text is available
-    // }
+
     if(completion) {
       console.log("Successfully fetched data from OpenAI");
       return content;
@@ -33,14 +26,12 @@ export const FetchConjugations = async (callback) => {
     }
   } catch (error) {
     console.error('Failed to fetch conjugations from OpenAI:', error);
-    callback(''); // Handle errors by calling back with empty string or appropriate error message
+    if (error.response && error.response.status === 429) {
+      console.error('Rate limit exceeded, retrying in 30 seconds...');
+      setTimeout(() => FetchConjugations(callback), 30000);
+    } else {
+      // Handle other types of errors
+    }
   }
 };
 export default FetchConjugations;
-
-
-
-
-
-
-
